5a71eb640d81d87dae99b3965e41fd342c9645dc
Wesley Lo <wesleylo1995@gmail.com>
2022-01-12 14:43:16 -0600

    problem 5 solved

--- arrayMethods.js
+++ arrayMethods.js
@@ -95,18 +95,20 @@ console.log(myStrongest)
 ////////// PROBLEM 5 //////////
 
 // Do not edit code below.
-// const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax":0.11},
-// {"price":80,"tax":0.11},{"price":69,"tax":0.06},{"price":68,"tax":0.14},
-// {"price":72,"tax":0.14},{"price":51,"tax":0.09},{"price":89,"tax":0.15},
-// {"price":48,"tax":0.13}];
+const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax":0.11},
+{"price":80,"tax":0.11},{"price":69,"tax":0.06},{"price":68,"tax":0.14},
+{"price":72,"tax":0.14},{"price":51,"tax":0.09},{"price":89,"tax":0.15},
+{"price":48,"tax":0.13}];
 // Do not edit code above.
 
 /*
-  Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate, hint: you'll need to do some multiplication). Your answer should be an array of numbers, one total for each order.
+  Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate, 
+  hint: you'll need to do some multiplication). Your answer should be an array of numbers, one total for each order.
 */
 
 // CODE HERE
-
+const orderTotals = orders.map(orders => orders.price + (orders.price * orders.tax))
+console.log(orderTotals);
 
 
 ////////// PROBLEM 6 //////////

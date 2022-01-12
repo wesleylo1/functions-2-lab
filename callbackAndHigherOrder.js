540285ebf6502edbe6870d60eb7caf2e25d69521
Wesley Lo <wesleylo1995@gmail.com>
2022-01-12 15:37:59 -0600

    problem 2 solved

--- callbackAndHigherOrder.js
+++ callbackAndHigherOrder.js
@@ -47,15 +47,17 @@ var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan']
 */
 
 // CODE HERE 
-
+function first(array,callback) {
+  callback(array[0])
+}
 
 // UNCOMMENT THE FUNCTION CALL BELOW
 // RUN THIS FILE WITH NODE
 // CHECK YOUR ANSWER
 
-// first(names, firstName => {
-//   console.log('The first name in names is ' + firstName)
-// })
+first(names, firstName => {
+  console.log('The first name in names is ' + firstName)
+})
 
 
 

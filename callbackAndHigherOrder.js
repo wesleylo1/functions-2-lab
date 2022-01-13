948af7dc6f55f31dfe4707b17a22fb3ae7055c1b
Wesley Lo <wesleylo1995@gmail.com>
2022-01-12 22:32:35 -0600

    problem 7 solved

--- callbackAndHigherOrder.js
+++ callbackAndHigherOrder.js
@@ -219,9 +219,9 @@ function getUserById(arr,id,callback) {
 // RUN THIS FILE WITH NODE
 // CHECK YOUR ANSWER
 
-// getUserById(users, '16t', user => {
-//   console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address) 
-// })
+getUserById(users, '16t', user => {
+  console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address) 
+})
 
 ////////// CHALLENGE //////////
 
